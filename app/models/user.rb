07ca2f721df07d54362767d85c66d3498a5226ea
CREMA <crema@FAC1s-MacBookAir.local>
2020-11-12 12:08:55 +0900

    Add comments to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_attached_file :image, styles: { medium: "300x300>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
   has_many :posts
+  has_many :comments
 end

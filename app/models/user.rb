d4a0db464be1360973565edcb209fa3d5f50d4d0
CREMA <crema@FAC1s-MacBookAir.local>
2020-11-12 11:51:45 +0900

    Setup paperclip for image uploading

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_attached_file :image, styles: { medium: "300x300>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   has_many :posts
 end
